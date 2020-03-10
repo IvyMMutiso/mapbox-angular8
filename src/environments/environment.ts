455f9d0e28f001e242e27b4e2ce579e68c93f026
Ivy Mutiso <ivy.mwonge@gmail.com>
2020-03-10 11:39:39 +0300

    Remove node-modules folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiaGlrYXlhaW8iLCJhIjoiY2s1NDJvYjl6MGRzeDNvcG1xeHQyMHJ2dyJ9.WPHFag6s5heCb9uItEOvjQ',
+  }
 };
 
 /*
