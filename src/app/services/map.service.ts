c02b1eb8eb18a174ef8305fb72dd38fa12e31759
Ivy Mutiso <ivy.mwonge@gmail.com>
2020-03-10 12:05:35 +0300

    Add data interface

---
+++ src/app/services/map.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+import * as mapboxgl from 'mapbox-gl';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MapService {
+
+  // constructor(private db: AngularFireDatabase) { }
+}
